497d759ae41ba4ddc221c5e1bd1fe49dcee064f8
yfang <yfang@hotpads.com>
2016-05-17 14:45:47 -0700

    fix setting.

---
+++ restsoap/src/main/java/com/restsoap/restsoap/config/guice/GuiceModule.java
@@ -0,0 +1,14 @@
+package com.restsoap.restsoap.config.guice;
+
+import com.google.inject.AbstractModule;
+
+public class GuiceModule extends AbstractModule{
+
+	@Override
+	protected void configure(){
+		// TODO Auto-generated method stub
+		//bind(A.class).to(B.class);
+
+	}
+
+}
---
+++ restsoap/src/main/java/com/restsoap/restsoap/config/guice/RestsoapGuiceServletContextListener.java
@@ -0,0 +1,14 @@
+package com.restsoap.restsoap.config.guice;
+
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+public class RestsoapGuiceServletContextListener extends GuiceServletContextListener{
+
+	@Override
+	protected Injector getInjector(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
